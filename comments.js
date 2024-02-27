8f4e1d40d266d1247f0e97e3d790cb08fd7245ef
Mohit Shaharwale <78531801+mohits2806@users.noreply.github.com>
2024-02-27 15:54:55 +0000

    Copilot new third commit

--- comments.js
+++ comments.js
@@ -1,58 +1,35 @@
-// Create a web server that listens for requests and serves up a web page
-// with a form for adding comments and a list of comments. The comments are
-// stored in a file on the server. When the page is requested, the server reads
-// the file and includes the comments in the page. When a comment is submitted,
-// the server appends it to the file and sends a redirect to the client so the
-// client can request the page again and see the new comment.
+// Create web server and listen for requests
+// Use express and body-parser
+// Use the comments API to manage comments
+// Use the comments API to manage comments
 
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var qs = require('querystring');
+var express = require('express');
+var bodyParser = require('body-parser');
+var comments = require('./comments-api');
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.txt');
+var app = express();
+app.use(bodyParser.json());
 
-function handleRequest(req, res) {
-  if (req.method === 'POST') {
-    var body = '';
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      var post = qs.parse(body);
-      fs.appendFile(COMMENTS_FILE, post.comment + '\n', function (err) {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        }
-        res.writeHead(302, { 'Location': '/' });
-        res.end();
-      });
-    });
-  } else {
-    fs.readFile(COMMENTS_FILE, function (err, data) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end('<html><head><title>Comments</title></head><body>' +
-              '<h1>Comments</h1>' +
-              '<ul>' +
-              data.toString().split('\n').map(function (comment) {
-                return '<li>' + comment + '</li>';
-              }).join('') +
-              '</ul>' +
-              '<form method="post" action="/">' +
-              '<input type="text" name="comment">' +
-              '<input type="submit" value="Add comment">' +
-              '</form>' +
-              '</body></html>');
-    });
-  }
-}
+app.get('/comments', function(req, res) {
+  comments.getAll(function(err, data) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
-var server = http.createServer(handleRequest);
-server.listen(3000, function () {
-  console.log('Listening on port 3000');
+app.post('/comments', function(req, res) {
+  comments.add(req.body, function(err, data) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000');
 });
